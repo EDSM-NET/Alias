4ae49bd37a76d6bb04470bf965a1ab01f7a04ab0
AnthorNet <anthor.net@gmail.com>
2017-09-05 13:39:01 +0200

    Add security aliases

---
+++ System/Security.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\System;
+use         EDSM\Alias;
+
+class Security extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name      = [
+         0  => 'None',
+         
+         1  => 'Low',
+         2  => 'Medium',
+         3  => 'High',
+    
+        10  => 'Anarchy',
+        11  => 'Lawless',
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     *     => str_replace(;)
+     *     => str_replace($system_security_)
+     *     => str_replace($galaxy_map_info_state_)
+     */
+    static protected $alias     = [
+        'low'           => 1,
+        'medium'        => 2,
+        'high'          => 3,
+        
+        'anarchy'       => 10,
+        'lawless'       => 11,
+    ];
+}
