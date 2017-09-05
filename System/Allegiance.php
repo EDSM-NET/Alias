985b7f6148143c6c69c38c15261b306fb1302d48
AnthorNet <anthor.net@gmail.com>
2017-09-05 09:57:04 +0200

    Add allegiance aliases

---
+++ System/Allegiance.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\System;
+use         EDSM\Alias;
+
+class Allegiance extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name      = [
+         0  => 'None',
+         
+         1  => 'Alliance',
+         2  => 'Empire',
+         3  => 'Federation',
+         4  => 'Independent',
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     */
+    static protected $alias     = [
+        'alliance'                          => 1,
+        'empire'                            => 2,
+        'federation'                        => 3,
+        'independent'                       => 4,
+        
+        // Bug on #19700/CM Draconis
+        'playerpilots'                      => 0,
+    ];
+}
