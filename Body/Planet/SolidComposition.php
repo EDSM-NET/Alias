1f0ff5de1577b68b56867e46500516fd3faf1082
AnthorNet <anthor.net@gmail.com>
2017-09-08 16:06:38 +0200

    Add solid composition alias

---
+++ Body/Planet/SolidComposition.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Body\Planet;
+use         EDSM\Alias;
+
+class SolidComposition extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name  = [
+         1          => 'Ice',
+         2          => 'Metal',
+         3          => 'Rock',
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     */
+    static protected $alias = [
+        'ice'       => 1,
+        'metal'     => 2,
+        'rock'      => 3,
+    ];
+}
