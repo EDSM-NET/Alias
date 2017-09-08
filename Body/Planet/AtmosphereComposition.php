085c66be240e2925c79826f3148e73216310eda0
AnthorNet <anthor.net@gmail.com>
2017-09-08 16:06:57 +0200

    Add Atmosphere composition alias

---
+++ Body/Planet/AtmosphereComposition.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Body\Planet;
+use         EDSM\Alias;
+
+class AtmosphereComposition extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name  = [
+         1      => 'Argon',
+         2      => 'Ammonia',
+         3      => 'Carbon dioxide',
+         4      => 'Hydrogen',
+         5      => 'Helium',
+         6      => 'Iron',
+         7      => 'Neon',
+         8      => 'Methane',
+         9      => 'Nitrogen',
+        10      => 'Oxygen',
+        11      => 'Silicates',
+        12      => 'Sulphur dioxide',
+        13      => 'Water',
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     */
+    static protected $alias = [
+        'argon'                         => 1,
+        'ammonia'                       => 2,
+        'carbon dioxide'                => 3,       'carbondioxide'     => 3,
+        'hydrogen'                      => 4,
+        'helium'                        => 5,
+        'iron'                          => 6,
+        'neon'                          => 7,
+        'methane'                       => 8,
+        'nitrogen'                      => 9,
+        'oxygen'                        => 10,
+        'silicates'                     => 11,
+        'sulphur dioxide'               => 12,      'sulphurdioxide'    => 12,
+        'water'                         => 13,
+    ];
+}
