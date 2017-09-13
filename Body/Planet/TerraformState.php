af35197e3702ef8f9e2ef87b3ac5780afb0eacdf
AnthorNet <anthor.net@gmail.com>
2017-09-13 14:54:28 +0200

    Add planet terraformState

---
+++ Body/Planet/TerraformState.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Body\Planet;
+use         EDSM\Alias;
+
+class TerraformState extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name  = [
+         0      => 'Not terraformable',
+    
+         1      => 'Candidate for terraforming',
+         2      => 'Terraformed',
+         3      => 'Terraforming',
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     */
+    static protected $alias = [
+        'terraformable'                 => 1,
+        'candidate for terraforming'    => 1,
+        
+        'terraformed'                   => 2,
+        'terraforming completed'        => 2,
+        
+        'terraforming'                  => 3,
+        'being terraformed'             => 3,
+    ];
+}
