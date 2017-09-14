fc6efa828e1608726e5ad33027c3d22341b716bf
AnthorNet <anthor.net@gmail.com>
2017-09-14 15:40:26 +0200

    Add bay listings

---
+++ Station/Outfitting/BayFighters.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Station\Outfitting;
+use         EDSM\Alias;
+
+class BayFighters extends Alias
+{
+    static protected $defaultName       = 0;
+    static protected $representation    = 'Fighters/bay';
+    
+    /**
+     * List of categories having bay
+     */
+    static protected $have          = [
+        'Fighter Hangar',
+    ];
+    
+    /**
+     * Number of fighters/bay by outfitting
+     */
+    static protected $name          = [
+        // Optional Internal > Fighter Hangar
+          5541  => 6,
+        
+          5551  => 8,
+        
+          5561  => 15,
+    ];
+}
