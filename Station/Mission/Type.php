9bb8a64ee086677fe2f7b0f7008edad64a0f1af4
AnthorNet <anthor.net@gmail.com>
2017-11-28 16:19:51 +0100

    Prepare mission types aliases

---
+++ Station/Mission/Type.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Station\Mission;
+use         EDSM\Alias;
+
+class Type extends Alias
+{
+    /**
+     * List of names used in game
+     */
+    static protected $name  = [
+        
+    ];
+    
+    /**
+     * List of aliases received from Frontier
+     * 
+     * Every aliases are sanitized before matching
+     *     => trim
+     *     => strtolower
+     */
+    static protected $alias = [
+        
+    ];
+}
