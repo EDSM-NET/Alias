a8436fcddd463e3404f48d305204335fd4937101
AnthorNet <anthor.net@gmail.com>
2018-01-26 13:36:41 +0100

    Add ship Shields/Armour

---
+++ Ship/Armour.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class Armour extends Alias
+{
+    static protected $defaultName       = null;
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 162,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 945,
+        
+        // Asp Explorer
+        128049303               => 378,
+        
+        // Asp Scout
+        128672276               => 324,
+        
+        // Beluga Liner
+        128049345               => 504,
+        
+        // Cobra MkIII
+        128049279               => 162,
+        
+        // Cobra MkIV
+        128672262               => 216,
+        
+        // Diamondback Explorer
+        128671831               => 162,
+        
+        // Diamondback Scout
+        128671217               => 270,
+        
+        // Dolphin
+        128049291               => 198,
+        
+        // Eagle
+        128049255               => 72,
+        
+        // Federal Assault Ship
+        128672145               => 540,
+        
+        // Federal Corvette
+        128049369               => 666,
+        
+        // Federal Dropship
+        128049321               => 540,
+        
+        // Federal Gunship
+        128672152               => 630,
+        
+        // Fer-de-Lance
+        128049351               => 405,
+        
+        // Hauler
+        128049261               => 90,
+        
+        // Imperial Clipper
+        128049315               => 486,
+        
+        // Imperial Courier
+        128671223               => 144,
+        
+        // Imperial Cutter
+        128049375               => 720,
+        
+        // Imperial Eagle
+        128672138               => 108,
+        
+        // Keelback
+        128672269               => 243,
+        
+        // Orca
+        128049327               => 396,
+        
+        // Python
+        128049339               => 468,
+        
+        // Sidewinder
+        128049249               => 108,
+        
+        // Type-6 Transporter
+        128049285               => 162,
+        
+        // Type-7 Transporter
+        128049297               => 306,
+        
+        // Type-9 Heavy
+        128049333               => 432,
+        
+        // Type-10 Defender
+        128785619               => 1044,
+        
+        // Viper MkIII
+        128049273               => 126,
+        
+        // Viper MkIV
+        128672255               => 270,
+        
+        // Vulture
+        128049309               => 288,
+    ];
+}
---
+++ Ship/Shields.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class Shields extends Alias
+{
+    static protected $defaultName       = null;
+    static protected $unit              = '%1$s ly';
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 78,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 362,
+        
+        // Asp Explorer
+        128049303               => 152,
+        
+        // Asp Scout
+        128672276               => 105,
+        
+        // Beluga Liner
+        128049345               => 92,
+        
+        // Cobra MkIII
+        128049279               => 78,
+        
+        // Cobra MkIV
+        128672262               => 92,
+        
+        // Diamondback Explorer
+        128671831               => 78,
+        
+        // Diamondback Scout
+        128671217               => 115,
+        
+        // Dolphin
+        128049291               => 143,
+        
+        // Eagle
+        128049255               => 78,
+        
+        // Federal Assault Ship
+        128672145               => 133,
+        
+        // Federal Corvette
+        128049369               => 516,
+        
+        // Federal Dropship
+        128049321               => 104,
+        
+        // Federal Gunship
+        128672152               => 186,
+        
+        // Fer-de-Lance
+        128049351               => 271,
+        
+        // Hauler
+        128049261               => 65,
+        
+        // Imperial Clipper
+        128049315               => 186,
+        
+        // Imperial Courier
+        128671223               => 230,
+        
+        // Imperial Cutter
+        128049375               => 703,
+        
+        // Imperial Eagle
+        128672138               => 104,
+        
+        // Keelback
+        128672269               => 99,
+        
+        // Orca
+        128049327               => 114,
+        
+        // Python
+        128049339               => 294,
+        
+        // Sidewinder
+        128049249               => 52,
+        
+        // Type-6 Transporter
+        128049285               => 77,
+        
+        // Type-7 Transporter
+        128049297               => 78,
+        
+        // Type-9 Heavy
+        128049333               => 88,
+        
+        // Type-10 Defender
+        128785619               => 99,
+        
+        // Viper MkIII
+        128049273               => 137,
+        
+        // Viper MkIV
+        128672255               => 103,
+        
+        // Vulture
+        128049309               => 293,
+    ];
+}
