ace696f3ac1651b19a231e9abb157272967b64ff
AnthorNet <anthor.net@gmail.com>
2018-01-26 13:37:12 +0100

    Add ship hull mass

---
+++ Ship/HullMass.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class HullMass extends Alias
+{
+    static protected $defaultName       = null;
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 35,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 400,
+        
+        // Asp Explorer
+        128049303               => 280,
+        
+        // Asp Scout
+        128672276               => 150,
+        
+        // Beluga Liner
+        128049345               => 1100,
+        
+        // Cobra MkIII
+        128049279               => 35,
+        
+        // Cobra MkIV
+        128672262               => 180,
+        
+        // Diamondback Explorer
+        128671831               => 35,
+        
+        // Diamondback Scout
+        128671217               => 298,
+        
+        // Dolphin
+        128049291               => 140,
+        
+        // Eagle
+        128049255               => 50,
+        
+        // Federal Assault Ship
+        128672145               => 480,
+        
+        // Federal Corvette
+        128049369               => 900,
+        
+        // Federal Dropship
+        128049321               => 580,
+        
+        // Federal Gunship
+        128672152               => 580,
+        
+        // Fer-de-Lance
+        128049351               => 250,
+        
+        // Hauler
+        128049261               => 14,
+        
+        // Imperial Clipper
+        128049315               => 400,
+        
+        // Imperial Courier
+        128671223               => 35,
+        
+        // Imperial Cutter
+        128049375               => 1100,
+        
+        // Imperial Eagle
+        128672138               => 50,
+        
+        // Keelback
+        128672269               => 180,
+        
+        // Orca
+        128049327               => 580,
+        
+        // Python
+        128049339               => 350,
+        
+        // Sidewinder
+        128049249               => 25,
+        
+        // Type-6 Transporter
+        128049285               => 155,
+        
+        // Type-7 Transporter
+        128049297               => 420,
+        
+        // Type-9 Heavy
+        128049333               => 1000,
+        
+        // Type-10 Defender
+        128785619               => 1200,
+        
+        // Viper MkIII
+        128049273               => 60,
+        
+        // Viper MkIV
+        128672255               => 190,
+        
+        // Vulture
+        128049309               => 230,
+    ];
+}
