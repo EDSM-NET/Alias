c4f628432bbcd571e9da324a56f5abe4a5e142c3
AnthorNet <anthor.net@gmail.com>
2018-01-26 11:56:25 +0100

    Add Manoeuvrability

---
+++ Ship/Manoeuvrability.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class Manoeuvrability extends Alias
+{
+    static protected $defaultName       = null;
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 4,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 1,
+        
+        // Asp Explorer
+        128049303               => 4,
+        
+        // Asp Scout
+        128672276               => 5,
+        
+        // Beluga Liner
+        128049345               => 2,
+        
+        // Cobra MkIII
+        128049279               => 4,
+        
+        // Cobra MkIV
+        128672262               => 5,
+        
+        // Diamondback Explorer
+        128671831               => 4,
+        
+        // Diamondback Scout
+        128671217               => 4,
+        
+        // Dolphin
+        128049291               => 3,
+        
+        // Eagle
+        128049255               => 7,
+        
+        // Federal Assault Ship
+        128672145               => 4,
+        
+        // Federal Corvette
+        128049369               => 2,
+        
+        // Federal Dropship
+        128049321               => 3,
+        
+        // Federal Gunship
+        128672152               => 1,
+        
+        // Fer-de-Lance
+        128049351               => 4,
+        
+        // Hauler
+        128049261               => 4,
+        
+        // Imperial Clipper
+        128049315               => 5,
+        
+        // Imperial Courier
+        128671223               => 4,
+        
+        // Imperial Cutter
+        128049375               => 0,
+        
+        // Imperial Eagle
+        128672138               => 5,
+        
+        // Keelback
+        128672269               => 2,
+        
+        // Orca
+        128049327               => 1,
+        
+        // Python
+        128049339               => 2,
+        
+        // Sidewinder
+        128049249               => 5,
+        
+        // Type-6 Transporter
+        128049285               => 3,
+        
+        // Type-7 Transporter
+        128049297               => 1,
+        
+        // Type-9 Heavy
+        128049333               => 0,
+        
+        // Type-10 Defender
+        128785619               => 0,
+        
+        // Viper MkIII
+        128049273               => 4,
+        
+        // Viper MkIV
+        128672255               => 3,
+        
+        // Vulture
+        128049309               => 5,
+    ];
+}
