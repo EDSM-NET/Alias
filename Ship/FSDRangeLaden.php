2e6b52ebe8f6de02bb4073fcd8618300d4f3eebe
AnthorNet <anthor.net@gmail.com>
2018-01-26 13:20:47 +0100

    Add ship FSD ranges

---
+++ Ship/FSDRangeLaden.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class FSDRangeLaden extends Alias
+{
+    static protected $defaultName       = null;
+    static protected $unit              = '%1$s ly';
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 8.47,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 8.52,
+        
+        // Asp Explorer
+        128049303               => 12.07,
+        
+        // Asp Scout
+        128672276               => 10.84,
+        
+        // Beluga Liner
+        128049345               => 8.79,
+        
+        // Cobra MkIII
+        128049279               => 8.47,
+        
+        // Cobra MkIV
+        128672262               => 9.78,
+        
+        // Diamondback Explorer
+        128671831               => 8.47,
+        
+        // Diamondback Scout
+        128671217               => 13.74,
+        
+        // Dolphin
+        128049291               => 10.11,
+        
+        // Eagle
+        128049255               => 8.27,
+        
+        // Federal Assault Ship
+        128672145               => 7.93,
+        
+        // Federal Corvette
+        128049369               => 6.03,
+        
+        // Federal Dropship
+        128049321               => 6.77,
+        
+        // Federal Gunship
+        128672152               => 6.54,
+        
+        // Fer-de-Lance
+        128049351               => 13.53,
+        
+        // Hauler
+        128049261               => 8.08,
+        
+        // Imperial Clipper
+        128049315               => 8.05,
+        
+        // Imperial Courier
+        128671223               => 7.69,
+        
+        // Imperial Cutter
+        128049375               => 7.57,
+        
+        // Imperial Eagle
+        128672138               => 8.03,
+        
+        // Keelback
+        128672269               => 9.34,
+        
+        // Orca
+        128049327               => 6.66,
+        
+        // Python
+        128049339               => 7.36,
+        
+        // Sidewinder
+        128049249               => 6.96,
+        
+        // Type-6 Transporter
+        128049285               => 10.07,
+        
+        // Type-7 Transporter
+        128049297               => 8.63,
+        
+        // Type-9 Heavy
+        128049333               => 6.62,
+        
+        // Type-10 Defender
+        128785619               => 5.87,
+        
+        // Viper MkIII
+        128049273               => 6.66,
+        
+        // Viper MkIV
+        128672255               => 9.69,
+        
+        // Vulture
+        128049309               => 7.75,
+    ];
+}
---
+++ Ship/FSDRangeUnladen.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class FSDRangeUnladen extends Alias
+{
+    static protected $defaultName       = null;
+    static protected $unit              = '%1$s ly';
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 9.12,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 9.41,
+        
+        // Asp Explorer
+        128049303               => 13.12,
+        
+        // Asp Scout
+        128672276               => 11.59,
+        
+        // Beluga Liner
+        128049345               => 9.01,
+        
+        // Cobra MkIII
+        128049279               => 9.12,
+        
+        // Cobra MkIV
+        128672262               => 10.46,
+        
+        // Diamondback Explorer
+        128671831               => 9.12,
+        
+        // Diamondback Scout
+        128671217               => 14.15,
+        
+        // Dolphin
+        128049291               => 10.67,
+        
+        // Eagle
+        128049255               => 8.47,
+        
+        // Federal Assault Ship
+        128672145               => 8.30,
+        
+        // Federal Corvette
+        128049369               => 6.31,
+        
+        // Federal Dropship
+        128049321               => 7.25,
+        
+        // Federal Gunship
+        128672152               => 6.66,
+        
+        // Fer-de-Lance
+        128049351               => 14.33,
+        
+        // Hauler
+        128049261               => 9.87,
+        
+        // Imperial Clipper
+        128049315               => 8.98,
+        
+        // Imperial Courier
+        128671223               => 8.83,
+        
+        // Imperial Cutter
+        128049375               => 8.21,
+        
+        // Imperial Eagle
+        128672138               => 8.22,
+        
+        // Keelback
+        128672269               => 10.94,
+        
+        // Orca
+        128049327               => 6.86,
+        
+        // Python
+        128049339               => 8.23,
+        
+        // Sidewinder
+        128049249               => 7.56,
+        
+        // Type-6 Transporter
+        128049285               => 12.39,
+        
+        // Type-7 Transporter
+        128049297               => 10.68,
+        
+        // Type-9 Heavy
+        128049333               => 7.71,
+        
+        // Type-10 Defender
+        128785619               => 6.97,
+        
+        // Viper MkIII
+        128049273               => 6.92,
+        
+        // Viper MkIV
+        128672255               => 10.36,
+        
+        // Vulture
+        128049309               => 7.93,
+    ];
+}
