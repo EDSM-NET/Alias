8cf8e06a002fed21f3fcb3cac47722c0b8991339
AnthorNet <anthor.net@gmail.com>
2018-01-26 13:36:57 +0100

    Add ship cargo capacity

---
+++ Ship/CargoCapacity.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class CargoCapacity extends Alias
+{
+    static protected $defaultName       = null;
+    static protected $unit              = '%1$s t';
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 6,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 114,
+        
+        // Asp Explorer
+        128049303               => 38,
+        
+        // Asp Scout
+        128672276               => 16,
+        
+        // Beluga Liner
+        128049345               => 44,
+        
+        // Cobra MkIII
+        128049279               => 6,
+        
+        // Cobra MkIV
+        128672262               => 18,
+        
+        // Diamondback Explorer
+        128671831               => 6,
+        
+        // Diamondback Scout
+        128671217               => 12,
+        
+        // Dolphin
+        128049291               => 14,
+        
+        // Eagle
+        128049255               => 2,
+        
+        // Federal Assault Ship
+        128672145               => 32,
+        
+        // Federal Corvette
+        128049369               => 76,
+        
+        // Federal Dropship
+        128049321               => 56,
+        
+        // Federal Gunship
+        128672152               => 16,
+        
+        // Fer-de-Lance
+        128049351               => 24,
+        
+        // Hauler
+        128049261               => 8,
+        
+        // Imperial Clipper
+        128049315               => 74,
+        
+        // Imperial Courier
+        128671223               => 12,
+        
+        // Imperial Cutter
+        128049375               => 164,
+        
+        // Imperial Eagle
+        128672138               => 2,
+        
+        // Keelback
+        128672269               => 42,
+        
+        // Orca
+        128049327               => 24,
+        
+        // Python
+        128049339               => 82,
+        
+        // Sidewinder
+        128049249               => 4,
+        
+        // Type-6 Transporter
+        128049285               => 50,
+        
+        // Type-7 Transporter
+        128049297               => 128,
+        
+        // Type-9 Heavy
+        128049333               => 220,
+        
+        // Type-10 Defender
+        128785619               => 220,
+        
+        // Viper MkIII
+        128049273               => 4,
+        
+        // Viper MkIV
+        128672255               => 18,
+        
+        // Vulture
+        128049309               => 8,
+    ];
+}
