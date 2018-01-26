8fc735a7885f00a1b32b964da07111cbbb206765
AnthorNet <anthor.net@gmail.com>
2018-01-26 11:56:43 +0100

    Add ship boost speed

---
+++ Ship/BoostSpeed.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Alias\Ship;
+use         EDSM\Alias;
+
+class BoostSpeed extends Alias
+{
+    static protected $defaultName       = null;
+    static protected $unit              = '%1$s m/s';
+    
+    /**
+     * Boost speed by ship
+     */
+    static protected $name  = [
+        // Adder
+        128049267               => 321,
+        
+        // Alliance Chieftain
+        128816574               => null,
+        
+        // Anaconda
+        128049363               => 244,
+        
+        // Asp Explorer
+        128049303               => 345,
+        
+        // Asp Scout
+        128672276               => 304,
+        
+        // Beluga Liner
+        128049345               => 273,
+        
+        // Cobra MkIII
+        128049279               => 321,
+        
+        // Cobra MkIV
+        128672262               => 402,
+        
+        // Diamondback Explorer
+        128671831               => 321,
+        
+        // Diamondback Scout
+        128671217               => 316,
+        
+        // Dolphin
+        128049291               => 361,
+        
+        // Eagle
+        128049255               => 349,
+        
+        // Federal Assault Ship
+        128672145               => 357,
+        
+        // Federal Corvette
+        128049369               => 257,
+        
+        // Federal Dropship
+        128049321               => 304,
+        
+        // Federal Gunship
+        128672152               => 282,
+        
+        // Fer-de-Lance
+        128049351               => 357,
+        
+        // Hauler
+        128049261               => 305,
+        
+        // Imperial Clipper
+        128049315               => 388,
+        
+        // Imperial Courier
+        128671223               => 380,
+        
+        // Imperial Cutter
+        128049375               => 323,
+        
+        // Imperial Eagle
+        128672138               => 397,
+        
+        // Keelback
+        128672269               => 303,
+        
+        // Orca
+        128049327               => 384,
+        
+        // Python
+        128049339               => 305,
+        
+        // Sidewinder
+        128049249               => 321,
+        
+        // Type-6 Transporter
+        128049285               => 355,
+        
+        // Type-7 Transporter
+        128049297               => 301,
+        
+        // Type-9 Heavy
+        128049333               => 201,
+        
+        // Type-10 Defender
+        128785619               => 219,
+        
+        // Viper MkIII
+        128049273               => 388,
+        
+        // Viper MkIV
+        128672255               => 342,
+        
+        // Vulture
+        128049309               => 348,
+    ];
+}
