5b11b18163842280f06b0e83a70a3983455e5aeb
AnthorNet <anthor.net@gmail.com>
2018-04-24 09:42:46 +0200

    Differentiate outposts

--- Station/Type.php
+++ Station/Type.php
@@ -20,6 +20,12 @@ class Type extends Alias
          3                      => 'Orbis Starport',
          
          4                      => 'Outpost',
+        41                      => 'Civilian Outpost',
+        42                      => 'Commercial Outpost',
+        43                      => 'Industrial Outpost',
+        44                      => 'Military Outpost',
+        45                      => 'Mining Outpost',
+        46                      => 'Scientific Outpost',
          
          5                      => 'Planetary Outpost',
          6                      => 'Planetary Port',
@@ -39,26 +45,39 @@ class Type extends Alias
         'coriolis'              => 1,
         'coriolis starport'     => 1,
         
+        
         'bernal'                => 2,
         'ocellus starport'      => 2,
+        
         
         'orbis'                 => 3,
         'orbis starport'        => 3,
         
+        
         'outpost'               => 4,
-        'civilian outpost'      => 4,
-        'commercial outpost'    => 4,
-        'industrial outpost'    => 4,
-        'military outpost'      => 4,
-        'mining outpost'        => 4,
-        'scientific outpost'    => 4,
+        
+        'civilian outpost'      => 41,
+        
+        'commercial outpost'    => 42,
+        
+        'industrial outpost'    => 43,
+        
+        'military outpost'      => 44,
+        
+        'mining outpost'        => 45,
+        
+        'scientific outpost'    => 46,
+        'outpostscientific'     => 46,
+        
         
         'surfacestation'        => 5,
         'crateroutpost'         => 5,
         'planetary outpost'     => 5,
         
+        
         'planetary port'        => 6,
         'craterport'            => 6,
+        
         
         'asteroidbase'          => 11,
         'megaship'              => 12,
