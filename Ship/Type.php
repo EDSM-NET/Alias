2501cd9994b4ca4d9ae34b16b57fd2176efd7c46
AnthorNet <anthor.net@gmail.com>
2018-04-25 10:00:44 +0200

    Reverse FD alias to make Coriolis conversion

--- Ship/Type.php
+++ Ship/Type.php
@@ -68,32 +68,32 @@ class Type extends Alias
         'anaconda'                          => 128049363,
         
         // Asp Explorer
-        'asp'                               => 128049303,
         'asp explorer'                      => 128049303,
+        'asp'                               => 128049303,
         
         // Asp Scout
-        'asp_scout'                         => 128672276,
         'asp scout'                         => 128672276,
+        'asp_scout'                         => 128672276,
         
         // Beluga Liner
-        'belugaliner'                       => 128049345,
         'beluga liner'                      => 128049345,
+        'belugaliner'                       => 128049345,
         
         // Cobra MkIII
-        'cobramkiii'                        => 128049279,
         'cobra mk. iii'                     => 128049279,
+        'cobramkiii'                        => 128049279,
         
         // Cobra MkIV
-        'cobramkiv'                         => 128672262,
         'cobra mk. iv'                      => 128672262,
+        'cobramkiv'                         => 128672262,
         
         // Diamondback Explorer
-        'diamondbackxl'                     => 128671831,
         'diamondback explorer'              => 128671831,
+        'diamondbackxl'                     => 128671831,
         
         // Diamondback Scout
-        'diamondback'                       => 128671217,
         'diamondback scout'                 => 128671217,
+        'diamondback'                       => 128671217,
         
         // Dolphin
         'dolphin'                           => 128049291,
@@ -102,48 +102,47 @@ class Type extends Alias
         'eagle'                             => 128049255,
         
         // Federal Assault Ship
-        'federation_dropship_mkii'          => 128672145,
         'federal assault ship'              => 128672145,
+        'federation_dropship_mkii'          => 128672145,
         
         // Federal Corvette
-        'federation_corvette'               => 128049369,
         'federal corvette'                  => 128049369,
+        'federation_corvette'               => 128049369,
         
         // Federal Dropship
-        'federation_dropship'               => 128049321,
         'federal dropship'                  => 128049321,
+        'federation_dropship'               => 128049321,
         
         // Federal Gunship
-        'federation_gunship'                => 128672152,
         'federal gunship'                   => 128672152,
+        'federation_gunship'                => 128672152,
         
         // Fer-de-Lance
-        'ferdelance'                        => 128049351,
         'fer-de-lance'                      => 128049351,
+        'ferdelance'                        => 128049351,
         
         // Hauler
         'hauler'                            => 128049261,
         
         // Imperial Clipper
-        'empire_trader'                     => 128049315,
         'imperial clipper'                  => 128049315,
+        'empire_trader'                     => 128049315,
         
         // Imperial Courier
-        'empire_courier'                    => 128671223,
         'imperial courier'                  => 128671223,
+        'empire_courier'                    => 128671223,
         
         // Imperial Cutter
-        'cutter'                            => 128049375,
         'imperial cutter'                   => 128049375,
+        'cutter'                            => 128049375,
         
         // Imperial Eagle
-        'empire_eagle'                      => 128672138,
         'imperial eagle'                    => 128672138,
-        
+        'empire_eagle'                      => 128672138,
         
         // Keelback
-        'independant_trader'                => 128672269,
         'keelback'                          => 128672269,
+        'independant_trader'                => 128672269,
         
         // Orca
         'orca'                              => 128049327,
@@ -155,28 +154,28 @@ class Type extends Alias
         'sidewinder'                        => 128049249,
         
         // Type-6 Transporter
-        'type6'                             => 128049285,
         'type 6 transporter'                => 128049285,
+        'type6'                             => 128049285,
         
         // Type-7 Transporter
-        'type7'                             => 128049297,
         'type 7 transporter'                => 128049297,
+        'type7'                             => 128049297,
         
         // Type-9 Heavy
-        'type9'                             => 128049333,
         'type 9 heavy'                      => 128049333,
+        'type9'                             => 128049333,
         
         // Type-10 Defender
-        'type9_military'                    => 128785619,
         'type 10 defender'                  => 128785619,
+        'type9_military'                    => 128785619,
         
         // Viper MkIII
-        'viper'                             => 128049273,
         'viper mk. iii'                     => 128049273,
+        'viper'                             => 128049273,
         
         // Viper MkIV
-        'viper_mkiv'                        => 128672255,
         'viper mk. iv'                      => 128672255,
+        'viper_mkiv'                        => 128672255,
         
         // Vulture
         'vulture'                           => 128049309,
