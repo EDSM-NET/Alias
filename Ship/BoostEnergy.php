0cf7b147398c460b82f7f56514c8ba8c9846b0f5
AnthorNet <anthor.net@gmail.com>
2018-06-06 09:01:29 +0200

    Add Booster Energy

--- Ship/BoostEnergy.php
+++ Ship/BoostEnergy.php
@@ -9,112 +9,112 @@
 namespace   Alias\Ship;
 use         EDSM\Alias;
 
-class BoostSpeed extends Alias
+class BoostEnergy extends Alias
 {
     static protected $defaultName       = null;
     static protected $unit              = '%1$s m/s';
     
     /**
-     * Boost speed by ship
+     * Boost energy by ship
      */
     static protected $name  = [
         // Adder
-        128049267               => 321,
+        128049267               => 9,
         
         // Alliance Chieftain
-        128816574               => 330,
+        128816574               => 19,
         
         // Anaconda
-        128049363               => 244,
+        128049363               => 27,
         
         // Asp Explorer
-        128049303               => 345,
+        128049303               => 13,
         
         // Asp Scout
-        128672276               => 304,
+        128672276               => 13,
         
         // Beluga Liner
-        128049345               => 273,
+        128049345               => 17,
         
         // Cobra MkIII
-        128049279               => 321,
+        128049279               => 10,
         
         // Cobra MkIV
-        128672262               => 402,
+        128672262               => 10,
         
         // Diamondback Explorer
-        128671831               => 321,
+        128671831               => 13,
         
         // Diamondback Scout
-        128671217               => 316,
+        128671217               => 10,
         
         // Dolphin
-        128049291               => 361,
+        128049291               => 10,
         
         // Eagle
-        128049255               => 349,
+        128049255               => 8,
         
         // Federal Assault Ship
-        128672145               => 357,
+        128672145               => 19,
         
         // Federal Corvette
-        128049369               => 257,
+        128049369               => 27,
         
         // Federal Dropship
-        128049321               => 304,
+        128049321               => 19,
         
         // Federal Gunship
-        128672152               => 282,
+        128672152               => 23,
         
         // Fer-de-Lance
-        128049351               => 357,
+        128049351               => 19,
         
         // Hauler
-        128049261               => 305,
+        128049261               => 7,
         
         // Imperial Clipper
-        128049315               => 388,
+        128049315               => 19,
         
         // Imperial Courier
-        128671223               => 380,
+        128671223               => 10,
         
         // Imperial Cutter
-        128049375               => 323,
+        128049375               => 23,
         
         // Imperial Eagle
-        128672138               => 397,
+        128672138               => 8,
         
         // Keelback
-        128672269               => 303,
+        128672269               => 10,
         
         // Orca
-        128049327               => 384,
+        128049327               => 16,
         
         // Python
-        128049339               => 305,
+        128049339               => 23,
         
         // Sidewinder
-        128049249               => 321,
+        128049249               => 7,
         
         // Type-6 Transporter
-        128049285               => 355,
+        128049285               => 10,
         
         // Type-7 Transporter
-        128049297               => 301,
+        128049297               => 10,
         
         // Type-9 Heavy
-        128049333               => 201,
+        128049333               => 19,
         
         // Type-10 Defender
-        128785619               => 219,
+        128785619               => 19,
         
         // Viper MkIII
-        128049273               => 388,
+        128049273               => 10,
         
         // Viper MkIV
-        128672255               => 342,
+        128672255               => 10,
         
         // Vulture
-        128049309               => 348,
+        128049309               => 16,
     ];
 }
