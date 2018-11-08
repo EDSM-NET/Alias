73ea04f5bc6edd6d7d979be33d6c5192d887f8f8
AnthorNet <anthor.net@gmail.com>
2018-11-08 15:34:12 +0100

    Remove old version of EliteLogAgent from EDDN

--- BlacklistSoftware.php
+++ BlacklistSoftware.php
@@ -7,9 +7,9 @@
 return [
     // Global are software banned from every interaction
     'GLOBAL'    => [
-    
+
     ],
-    
+
     // EDDN only banned software
     'EDDN'      => [
         350,        // ED-IBE (API) 0.5.7
@@ -18,14 +18,49 @@ return [
         354,        // ED-IBE (API) 0.6.0
         364,        // ED-IBE (API) 0.6.1
         373,        // ED-IBE (API) 0.6.3
-        
+
         970,        // EDDI 3.0.1-rc2
         972,        // EDDI 3.0.1-rc3
         983,        // EDDI 3.0.1-rc4
+
+        937,        // EliteLogAgent 0.2.0.0
+        960,        // EliteLogAgent 0.3.0.105
+        961,        // EliteLogAgent 0.5.0.140
+        962,        // EliteLogAgent 0.5.0.143
+        965,        // EliteLogAgent 0.5.0.147
+        985,        // EliteLogAgent 0.5.1.185
+        997,        // EliteLogAgent 0.5.1.190
+        1001,       // EliteLogAgent 0.5.1.175
+        1002,       // EliteLogAgent 0.5.1.202
+        1006,       // EliteLogAgent 0.5.1.204
+        1007,       // EliteLogAgent 0.5.1.209
+        1010,       // EliteLogAgent 0.5.1.211
+        1014,       // EliteLogAgent 0.5.1.215
+        1015,       // EliteLogAgent 0.5.1.218
+        1016,       // EliteLogAgent 0.5.1.220
+        1017,       // EliteLogAgent 0.5.1.222
+        1021,       // EliteLogAgent 0.6.0.225
+        1022,       // EliteLogAgent 0.6.0.228
+        1030,       // EliteLogAgent 0.6.0.239
+        1034,       // EliteLogAgent 0.6.0.241
+        1035,       // EliteLogAgent 0.6.0.243
+        1051,       // EliteLogAgent 0.6.0.247
+        1079,       // EliteLogAgent 0.6.0.248
+        1084,       // EliteLogAgent 0.6.0.256
+        1094,       // EliteLogAgent 0.6.2.262
+        1108,       // EliteLogAgent 0.6.3.279
+        1109,       // EliteLogAgent 0.6.2.0
+        1110,       // EliteLogAgent 0.6.3.280
+        1113,       // EliteLogAgent 0.6.3.282
+        1131,       // EliteLogAgent 0.6.3.294
+        1141,       // EliteLogAgent 0.7.0.308
+        1148,       // EliteLogAgent 0.8.0.0
+        1149,       // EliteLogAgent 0.8.0.313
+        1154,       // EliteLogAgent 0.8.1.333
     ],
-    
+
     // Journal Event (Scan) only ban
     'Scan'      => [
-    
+
     ],
 ];
