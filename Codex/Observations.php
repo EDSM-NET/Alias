e7e1b38e9a8ddb51ee614c7ad597cd3f183d249f
AnthorNet <anthor.net@gmail.com>
2019-01-18 10:10:12 +0100

    More codex!

--- Codex/Observations.php
+++ Codex/Observations.php
@@ -15,10 +15,23 @@ class Observations extends Alias
      * List of names used in game
      */
     static protected $name  = [
+        21139   => [
+            'Winthin nebula types: Any.',
+            'Locations found: Lagrange points.',
+        ],
+
         22191   => [
             'In systems with star types: T.',
             'Locations found: Lagrange points, Rings, Belts.',
             'Not in systems with star types: W, WN, WNC, WC, WO.',
-        ]
+        ],
+
+        22491   => [
+            'In systems with star types: Y.',
+            'In systems with planet types: Terrestrial Earth-Like, Terrestrial Water, Gas Giant with Water based Life, Water Giant with Water based Life, Class II Gas Giant, Water Giant.',
+            'Locations found: Lagrange points, Rings, Belts.',
+            'Not in systems with star types: W, WN, WNC, WC, WO.',
+
+        ],
     ];
 }
--- Codex/Traits.php
+++ Codex/Traits.php
@@ -14,6 +14,7 @@ class Traits extends Alias
      */
     static protected $name  = [
          11                                 => 'Releases spores if approached without lights.',
+         14                                 => 'Releases spores if scanned.',
 
          51                                 => 'Moves towards ship if approached.',
          52                                 => 'Moved towards ship if approached with lights on.',
@@ -21,10 +22,12 @@ class Traits extends Alias
          54                                 => 'Moves towards ship if scanned.',
 
         101                                 => 'Turns towards nearby light sources.',
+        102                                 => 'Turns towards nearby light sources, and releases spores.',
 
         151                                 => 'Turns towards ship if approached.',
         153                                 => 'Turns towards ship if approached.', // Same?!
 
+        201                                 => 'Rapidly heats the surrounding space if approached.',
         202                                 => 'Interferes with ship systems and drains shields if approached.',
         203                                 => 'Releases destructive shockwaves if approached.',
 
@@ -35,6 +38,16 @@ class Traits extends Alias
         351                                 => 'Retreats rapidly if approached.',
 
         401                                 => 'Moves if approached with lights on.',
+
+        451                                 => 'Releases spores if approached.',
+
+        501                                 => 'Turns towards nearest star.',
+
+        551                                 => 'Moves if approached with lights on.',
+
+        601                                 => 'Approaches any nearby objects.',
+
+        651                                 => 'Approaches and damages nearby objects.',
     ];
 
     /**
@@ -46,6 +59,7 @@ class Traits extends Alias
      */
     static protected $alias = [
         'o_l_emit01_appro'                  => 11,
+        'o_l_emit04_appro'                  => 14,
 
         'o_s_appro01_appro'                 => 51,
         'o_s_appro02_appro'                 => 52,
@@ -53,10 +67,12 @@ class Traits extends Alias
         'o_s_appro04_appro'                 => 54,
 
         'o_l_turn01_observe'                => 101,
+        'o_l_turn02_observe'                => 102,
 
         's_s_turn01_observe'                => 151,
         's_s_turn03_observe'                => 153,
 
+        'p_l_aefect01'                      => 201,
         'p_l_aefect02'                      => 202,
         'p_l_aefect03'                      => 203,
 
@@ -67,5 +83,15 @@ class Traits extends Alias
         's_s_move_jet'                      => 351,
 
         'p_l_movearnd03'                    => 401,
+
+        's_s_cldatck01_cld'                 => 451,
+
+        'o_l_turn01_idle'                   => 501,
+
+        'p_l_movearnd01'                    => 551,
+
+        'o_l_atck01_appro'                  => 601,
+
+        'o_l_atck01_atck'                   => 651,
     ];
 }
